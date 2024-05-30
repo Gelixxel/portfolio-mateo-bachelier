80681b5aeb5806fa7d1e71bda6fcc2f17b03fcb9
BACHELIER Matéo <mateo.bachelier@gmail.com>
2024-05-31 00:19:53 +0200

    feat(main): fade-in anims

---
+++ scripts/fadeAnim.js
@@ -0,0 +1,23 @@
+// .section fade-in function
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('.section');
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
